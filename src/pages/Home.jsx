7f2c94a6a2d8020dddd993b5e5f5016d69fd4124
Soron007 <mitrasouvik123@gmail.com>
2024-08-08 13:42:38 +0530

    navbar WIP, logo remaining

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
+import HeroSection from "../components/HeroSection"
 
 
 
 const Home = () => {
     return (
-        <>
+        <div className="mt-36">
+            <HeroSection />
 
-            <div>
-                Home!
-            </div>
-
-        </>
+        </div>
     )
 }
 
