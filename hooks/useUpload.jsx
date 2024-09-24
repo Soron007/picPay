7d9c65114710f3a540d1c587320db0bfff5fa572
Souvik <mitrasouvik123@gmail.com>
2024-09-24 11:47:58 +0530

    formatted useUpload hook

--- hooks/useUpload.jsx
+++ hooks/useUpload.jsx
@@ -1,41 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 
+const useUpload = async ({ image, onUploadProgress }) => {
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", image);
+      formData.append(
+        "upload_preset",
+        import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
+      );
+      formData.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY);
 
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: onUploadProgress,
+        withCredentials: false,
+      };
 
-const useUpload = async({image, onUploadProgress}) => {
+      const res = await axios.post(
+        `https://api.cloudinary.com/v1_1/${
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/image/upload`,
+        formData,
+        config
+      );
 
-    const upload = async () => {
-        try {
-            const formData = new FormData();
-            formData.append("file", image);
-            formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-            formData.append("api_key", import.meta.env.VITE_CLOUDINARY_API_KEY)
+      const data = await res.data;
 
-            const config = {
-                headers: {
-                    "Content-Type": "multipart/form-data",
+      if (!data) {
+        return console.log("Image upload failed");
+      }
 
-                },
-                onUploadProgress: onUploadProgress,
-                withCredentials: false,
-            }
-
-            const res = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`, formData, config)
-
-            const data = await res.data;
-
-            if(!data){
-                return console.log("Image upload failed");
-            }
-
-            return data;
-        } catch (error) {
-            return error.message;
-        }
+      return data;
+    } catch (error) {
+      return error.message;
     }
+  };
 
-    const {public_id, secure_url} = await upload();
-    return {public_id, secure_url};
-}
+  const { public_id, secure_url } = await upload();
+  return { public_id, secure_url };
+};
 
 export default useUpload;
